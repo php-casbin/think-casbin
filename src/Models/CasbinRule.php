f3404160fea32a34997a99c059aad38ae3719acb
Thans <360641274@qq.com>
2018-12-26 19:45:35 +0800

    修复表前缀读取

--- src/Models/CasbinRule.php
+++ src/Models/CasbinRule.php
@@ -12,6 +12,8 @@ class CasbinRule extends Model
 
         //TODO:初始化内容
         $this->connection = config('casbin.database.connection') ?: '';
+        
+        $this->table = $this->connection ? config('database.'.config('casbin.database.connection').'.prefix') : config('database.prefix');
 
         $this->table = config('casbin.database.casbin_rules_table');
     }
