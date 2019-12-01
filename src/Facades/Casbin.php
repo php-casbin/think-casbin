9658c3285e5b9c9d5bd59f9238f8d5a726d71d86
Tinywan <756684177@qq.com>
2019-12-01 20:34:48 +0800

    1、添加静态RBAC API方法，方便IDE自动提示调用 2、添加阿里云composer源，方便安装扩展

--- src/Facades/Casbin.php
+++ src/Facades/Casbin.php
@@ -7,6 +7,28 @@ use think\Container;
 use Casbin\Enforcer;
 use Casbin\Model\Model;
 
+
+/**
+ * @see Enforcer
+ * @mixin Enforcer
+ * @method mixed GetRolesForUser(string $username) static 获取用户具有的角色
+ * @method mixed GetUsersForRole(string $role) static 获取具有角色的用户
+ * @method mixed HasRoleForUser(string $username, string $role) static 确定用户是否具有角色
+ * @method mixed AddRoleForUser(string $username, string $role) static 为用户添加角色
+ * @method mixed DeleteRoleForUser(string $username, string $role) static 删除用户的角色
+ * @method mixed DeleteRolesForUser(string $username) static 删除用户的所有角色
+ * @method mixed DeleteUser(string $username) static 删除一个用户
+ * @method mixed DeleteRole(string $role) static 删除一个角色
+ * @method mixed DeletePermission(string $policy) static 删除权限
+ * @method mixed AddPermissionForUser(string $username,string $policy) static 为用户或角色添加权限
+ * @method mixed DeletePermissionForUser(string $username,string $policy) static 删除用户或角色的权限
+ * @method mixed DeletePermissionsForUser(string $username) static 删除用户或角色的权限
+ * @method mixed GetPermissionsForUser(string $username) static 获取用户或角色的权限
+ * @method mixed HasPermissionForUser(string $username,string $policy) static 确定用户是否具有权限
+ * @method mixed GetImplicitRolesForUser(string $username) static 获取用户具有的隐式角色
+ * @method mixed GetImplicitPermissionsForUser(string $username) static 获取用户具有的隐式角色
+ */
+
 class Casbin extends Facade
 {
     protected static function getFacadeClass()
